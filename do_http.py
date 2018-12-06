096f534da3405187ec1c78bb93b32441f75506a6
toddlerya <toddlerya@gmail.com>
2018-12-06 23:25:45 +0800

    优化线程日志打印.

--- do_http.py
+++ do_http.py
@@ -29,7 +29,10 @@ class FasterThread(threading.Thread):
         self.kwargs = kwargs
 
     def run(self):
+        print 'start', self.func.__name__, threading.current_thread().getName()
         self.func(*self.args, **self.kwargs)
+        print 'finish', self.func.__name__, threading.current_thread().getName()
+
 
 
 def get_json_data(number, *args):
@@ -49,15 +52,13 @@ def get_json_data(number, *args):
                 print 'error column!', k
 
 
-def worker(col_list):
-    print 'start download'
+def download(col_list):
     while True:
         if not task_queue_1.empty():
             task = task_queue_1.get()
             get_json_data(task, col_list)
             task_queue_1.task_done()
         else:
-            print 'finish download'
             break
 
 
@@ -65,7 +66,6 @@ def load2db(commit_count=100):
     db = DataBase()
     if data_queue.empty():
         time.sleep(0.1)
-    print 'start_load2db'
     count = 0
     while True:
         if not data_queue.empty():
@@ -87,24 +87,24 @@ def load2db(commit_count=100):
                 db.conn.commit()
         else:
             db.conn.commit()
-            print 'load done! total {} line'.format(count)
+            print 'total {} line'.format(count)
             break
 
 
 if __name__ == '__main__':
-
+    print time.ctime()
     start = time.time()
     download_threads = []
-    for _ in range(200):
+    for _ in range(20):
         # number = random.randint(1, 10)
         task_queue_1.put(1000)
 
-    for i in range(5):
-        my_thread = FasterThread(worker, ['name', 'age', 'email'])
+    for i in range(15):
+        my_thread = FasterThread(download, ['name', 'age', 'email'])
         my_thread.start()
         download_threads.append(my_thread)
 
-    load_thread = FasterThread(load2db, 50000)
+    load_thread = FasterThread(load2db, 10000)
     load_thread.start()
 
     for thread in download_threads:
@@ -117,3 +117,4 @@ if __name__ == '__main__':
     end = time.time()
 
     print 'use {} second.'.format(end - start)
+    print time.ctime()
